f0f70104fd013c4356a0f7b22018ca465bd760ed
Mixel-MurP-427 <connor.mullarky@outlook.com>
2025-04-22 21:42:33 -0700

    created some experimental xml paths. do they work?

--- read_Spotify_playlist.py
+++ read_Spotify_playlist.py
@@ -5,31 +5,42 @@ from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
-driver.get("https://open.spotify.com/playlist/4LFw9FLn4MM50AnEHh84nV") #put the Spotify playlist url here
+driver.get("https://open.spotify.com/playlist/4rjTXDEitThyBbXnfRJdOV") #put the Spotify playlist url here
 assert "Spotify" in driver.title and "playlist" in driver.title
 
 
-#constant
-songs_path = "/html/body/div[5]/div/div[2]/div[5]/div/div[2]/div[2]/div/main/section/div[2]/div[3]/div/div[1]/div/div[2]/div[2]" #parent element of all songs
-something_ = "/html/body/div[5]/div/div[2]/div[5]/div/div[2]/div[2]/div/main/section/div[2]/div[3]/div/div[1]/div/div[2]/div[2]/div[1]"
+playlist = {}
 
-song_elements = driver.find_elements(By.XPATH, f"{songs_path}/div")
-print(len(song_elements))
+#constants
+#songs_path = "/html/body/div[5]/div/div[2]/div[5]/div/div[2]/div[2]/div/main/section/div[2]/div[3]/div/div[1]/div/div[2]/div[2]" #parent element of all songs
+song_path = '//div[@data-testid="tracklist-row"]'
+index_path = '/div/div/span'
+title_path = '/div/div/a/div'
+artist_path = 'div/div/span/div/a'
+album_path = '/div/span/a'
+
+for _ in range(40):
+    time.sleepms(250)
+    song_elements = driver.find_elements(By.XPATH, f"{song_path}")
+    print(len(song_elements))
 
 
 
-playlist = []
+
+"""
 for songElem in []:
 
     elem = driver.find_element(By.XPATH, "/html/body/div[5]/div/div[2]/div[5]/div/div[2]/div[2]/div/main/section/div[2]/div[3]/div/div[1]/div/div[2]/div[2]/div[1]")
     elem.click()
     assert "No results found." not in driver.page_source
     time.sleep(2)
+"""
 
 
 
 driver.close()
-
+print('closed successfully!')
+"""
 #save data
 with open('playlist.json', 'w') as myFile:
-    json.dump(playlist, myFile)
+    json.dump(playlist, myFile)"""
