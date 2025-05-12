f764fde7eb7721b752998f28a9f4fa9e162b8941
Mixel-MurP-427 <connor.mullarky@outlook.com>
2025-05-11 21:43:08 -0700

    Finished read_Spotify_playlist.py Minor config update. Removed autoscroll feature.

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
+#you can edit the values in this dictionary to configure your desired features.
 config_settings = {
-    "autoscroll": False, #if false, then the user may need to scroll manually to ensure all songs are loaded
+#    "autoscroll": True, #if false, then the user may need to scroll manually to ensure all songs are loaded
     "initial wait time": 1, #time to wait before starting the scan, to allow the page to load
     "iteration pause time": 0.25, #time to wait between each scan iteration, so it doesn't loop too fast
     "scan iterations": 5, #number of times to loop scanner
     "auto-save each iteration": True #if true then will write to playlist.json after each iteration. If false then read_Spotlist will return the playlist as a dictionary and the user can save it however they want.
 }
 
-assert type(config_settings["autoscroll"]) == bool, "Autoscroll must be a boolean."
+#assert type(config_settings["autoscroll"]) == bool, "Autoscroll must be a boolean."
 assert config_settings["initial wait time"] >= 0, "Initial wait time cannot be negative."
 assert config_settings["iteration pause time"] >= 0, "Iteration pause time cannot be negative."
 assert config_settings["scan iterations"] > 0, "Scan iterations must be a positive integer."
--- read_Spotify_playlist.py
+++ read_Spotify_playlist.py
@@ -1,7 +1,7 @@
 import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 import time
 
 #constants
@@ -26,7 +26,7 @@ def read_Spotlist(rsURL, rsConfig):
         time.sleep(rsConfig["iteration pause time"])
 
         song_elements = driver.find_elements(By.XPATH, song_path)
-        #print(len(song_elements))
+
         for song_element in song_elements:
             try:
                 song_info = {'title': song_element.find_element(By.XPATH, title_path).text,
@@ -35,19 +35,14 @@ def read_Spotlist(rsURL, rsConfig):
                 } 
                 index = int(song_element.find_element(By.XPATH, index_path).text)
 
-            except (NoSuchElementException, ValueError):
-                rsErrorCount += 1
+            except (NoSuchElementException, StaleElementReferenceException, ValueError):
+                rsErrorCount += 1 #this line of code is basically worthless
             else:
                 playlist[index] = song_info
-                #print(index, song_info)
 
         if rsConfig["auto-save each iteration"]:
             with open('playlist.json', 'w') as myFile:
                 json.dump(playlist, myFile)
-
-        if rsConfig["autoscroll"]:
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(rsConfig["iteration pause time"])
 
 
     driver.close()
@@ -55,5 +50,3 @@ def read_Spotlist(rsURL, rsConfig):
     print(f"Done!\nNumber of songs found: {len(playlist)}\nNumber of uncaught elements: {rsErrorCount}")
     if not rsConfig["auto-save each iteration"]:
         return playlist
-
-#TODO readme.md, at end of program print a report of the number of songs grabbed and if there are holes, perhaps autoscrolling, config vars at top with initial wait time, re-scan wait time, scan window time, boolean auto-save each scan, etc.
